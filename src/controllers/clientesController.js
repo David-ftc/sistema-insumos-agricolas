959c8b41cff1abb82ea5616db434408a9f3a547f
David Tarapues <david.tarapues@upec.edu.ec>
2026-04-21 20:10:52 -0500

    Agregando modulos de Inventario y Clientes - Trabajo en equipo

---
+++ src/controllers/clientesController.js
@@ -0,0 +1,6 @@
+// Modulo de Clientes - James Casanova
+function registrarCliente(req, res) {
+  console.log("Registrando nuevo cliente/proveedor...");
+  return { mensaje: "Cliente guardado correctamente" };
+}
+module.exports = { registrarCliente };
